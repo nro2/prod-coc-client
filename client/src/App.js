74c4a825820ebc715cc021b285ccab14cc4e6ca1
schwarz117 <cschwarz@pdx.edu>
2019-11-24 10:01:34 -0800

    Make Two Buttons Submit/Get

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: '',
+      getText: '',
+    };
+  }
+  handleSubmit(e) {
+    this.setState({ inputText: e.target.value });
+  }
+
+  getClick() {
+    this.setState({ getText: this.state.inputText });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Submit/Get</h1>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <input type="text" placeholder="First Name" name="name" />
+          </p>
+          <p>
+            <button>Submit</button>
+          </p>
+        </form>
+
+        <button type="submit" onClick={this.getClick}>
+          Get
+        </button>
+        <p>{this.state.getText}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
