18919bdfe4f3ce2672a5dcc9bebafbf195564ca5
schwarz117 <cschwarz@pdx.edu>
2019-11-24 13:58:16 -0800

    Add bind functions, state function

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,71 @@
 import React, { Component } from 'react';
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       inputText: '',
-      getText: '',
+      buffer: '',
+      userMessage: 'init',
+      pocGetResult: 'none',
     };
+    this.managesubmit = this.manageSubmit.bind(this);
+    this.performSubmit = this.performSubmit.bind(this);
   }
-  handleSubmit(e) {
-    this.setState({ inputText: e.target.value });
-  }
-
-  getClick() {
-    this.setState({ getText: this.state.inputText });
+  handleSubmit() {
+    this.setState({ inputText: '', userMessage: this.state.buffer });
   }
 
   render() {
     return (
-      <div>
-        <h1>Submit/Get</h1>
-        <form onSubmit={this.handleSubmit}>
+      <React.Fragment>
+        <form onSubmit={this.manageSubmit}>
+          <h1>Submit/Get</h1>
           <p>
-            <input type="text" placeholder="First Name" name="name" />
+            <input
+            type="text"
+            placeholder="First Name"
+            name="name"
+            value={this.state.inputText}
+            onChange={this.performSubmit}
+             />
           </p>
           <p>
-            <button>Submit</button>
+            <button
+            value="Submit"
+            onClick={() => this.handleSubmit()}>Submit
+            </button>
           </p>
         </form>
+        <p>
+          {this.createGetButton()}
+          <h2>Get Result:</h2>
+          {this.state.pocGetResult}
+        </p>
+      </React.Fragment>
+    );
+  }
+  manageSubmit(event){
+    event.preventDefault();
+  }
+  createGetButton(){
+    return(
+      <button value="GET" onClick={() => this.updateState()}>
+      GET
+      </button>
+    );
+  }
 
-        <button type="submit" onClick={this.getClick}>
-          Get
-        </button>
-        <p>{this.state.getText}</p>
-      </div>
+  performSubmit(data){
+    this.setState({ buffer: data.target.value, inputText: data.target.value})
+  }
+
+  updateState(){
+    return(
+      <React.Fragment>
+      {this.setState({ pocGetResult: this.state.userMessage})}
+      </React.Fragment>
     );
   }
 }
