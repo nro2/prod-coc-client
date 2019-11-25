199c110c9c3e65ffe6e1d0d9a483a0b55aee1667
Josh Wolsborn <wolsborn@pdx.edu>
2019-11-24 21:02:47 -0800

    CF1-40 Example Poc Branch Create Home component Create top-bar navigator

---
+++ client/src/Home.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class Home extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        return(
+            <div className="Home">
+                <h1>Welcome to the CoC App Home Page!</h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
