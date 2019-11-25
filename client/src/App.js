6bd8ab6ab46e59c1f3b06dce927f059526c54169
Josh Wolsborn <wolsborn@pdx.edu>
2019-11-24 16:47:20 -0800

    CF1-40 Example Poc Branch Remove unnecessary boilerplate from frontend Modify get endpoint to retrieve all user data

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: "",
+      text2: ""
+    };
+
+    this.getItem = this.getItem.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+
+  getItem(e){
+    axios.get('http://127.0.0.1:8080').then((response)=>{
+      let newText = response.data.firstName;
+      this.setState({
+        text: newText
+      });
+    });
+
+    this._aName.value = "";
+
+    e.preventDefault()
+  }
+
+  addItem(e){
+    axios.post('http://127.0.0.1:8080', {firstName: this._firstName.value, lastName: this._lastName.value, phoneNum:this._phoneNum.value}).then((response)=>{
+      let newText = response.data;
+      this.setState({
+        text2: newText
+      });
+      this._firstName.value = "";
+      this._lastName.value = "";
+      this._phoneNum.value = "";
+    });
+
+    e.preventDefault()
+  }
+
+
+
+  render() {
+    return (
+        <div className="App">
+          <div className="header">
+            <form onSubmit={this.addItem}>
+              <p>{this.state.text2}</p>
+              <input ref = {(a) => this._firstName = a}
+                     placeholder="First Name">
+              </input>
+              <input ref = {(a) => this._lastName = a}
+                     placeholder="Last Name">
+              </input>
+              <input ref = {(a) => this._phoneNum = a}
+                     placeholder="Phone Number">
+              </input>
+              <button type="submit">Post</button>
+            </form>
+            <form onSubmit={this.getItem}>
+              <p>{this.state.text}</p>
+              <input ref = {(a) => this._aName =a}
+                         placeholder="Enter first name">
+              </input>
+
+              <button type="submit">Get</button>
+            </form>
+            </div>
+        </div>
+    );
+  }
 }
 
 export default App;
