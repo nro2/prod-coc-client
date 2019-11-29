29e24018af563341e6e4b4d29b9464a810d7541a
Josh Wolsborn <wolsborn@pdx.edu>
2019-11-29 06:51:07 -0800

    CF1-40 Example Poc Branch Run linter/formatter

--- client/src/components/HomeComponent.js
+++ client/src/components/HomeComponent.js
@@ -1,17 +1,17 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
 
 class HomeComponent extends Component {
-    constructor(props){
-        super(props)
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return(
-            <div className="Home">
-                <h1>Welcome to the CoC App Home Page!</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="Home">
+        <h1>Welcome to the CoC App Home Page!</h1>
+      </div>
+    );
+  }
 }
 
 export default HomeComponent;
