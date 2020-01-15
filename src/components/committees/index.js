2eab3b1d01de1fa5a431f33feb4bec0cf5d07156
michaelheyman <mitch.heyman@gmail.com>
2020-01-14 16:05:03 -0800

    CF1-54 Add committee view
    Documentation on select:
    https://ant.design/components/select/

--- src/components/committees/index.js
+++ src/components/committees/index.js
@@ -1,52 +1,89 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Empty, Select } from 'antd';
+import './committees.css';
+
+const { Option } = Select;
 
 class Committees extends Component {
   constructor(props) {
     super(props);
     this.state = {
       committees: [],
-      text: '',
+      errorMessage: '',
+      errorCode: '',
+      loading: true,
     };
 
-    this.getCommittees = this.getCommittees.bind(this);
-    this.createCommittee = this.createCommittee.bind(this);
+    this.fetchCommittees = this.fetchCommittees.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  getCommittees() {
+  fetchCommittees() {
     axios
-      .get('http://127.0.0.1:8080/committees')
+      .get('http://localhost:8080/committees')
       .then(response => {
-        console.log(response.data);
-        let committeeArray = response.data;
         this.setState({
-          committees: committeeArray,
+          committees: response.data,
+          loading: false,
+          selected: 0,
         });
       })
       .catch(err => {
         this.setState({
-          text: 'Could not retrieve committees',
+          errorMessage: err,
         });
-        console.log(err);
       });
   }
 
-  createCommittee(item) {
-    return <li key={item.key}>{item.committee}</li>;
+  handleChange(value) {
+    this.setState({
+      selected: value,
+    });
   }
 
   componentDidMount() {
-    this.getCommittees();
+    this.fetchCommittees();
   }
 
   render() {
-    let items = this.state.committees;
-    let listItems = items.map(this.createCommittee);
+    const options = this.state.committees.map(committee => (
+      <Option key={committee.committee_id} value={committee.committee_id}>
+        {committee.name}
+      </Option>
+    ));
+
     return (
-      <div className="Committees">
-        <h1>List of Committees</h1>
-        <p>{this.state.text}</p>
-        {listItems}
+      <div>
+        <div>
+          <Select
+            showSearch
+            style={{ width: 300 }}
+            placeholder="Select a committee"
+            optionFilterProp="children"
+            onChange={this.handleChange}
+            filterOption={(input, option) =>
+              option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+            dropdownMatchSelectWidth={false}
+            size="large"
+            loading={this.state.loading}
+          >
+            {options}
+          </Select>
+        </div>
+        <div>
+          {this.state.selected === 0 ? (
+            <Empty />
+          ) : (
+            // TODO: display this committee data in a nicer way
+            JSON.stringify(
+              this.state.committees.find(
+                committee => committee.committee_id === this.state.selected
+              )
+            )
+          )}
+        </div>
       </div>
     );
   }
