25da77846992b0b99271fae5a0a91d0da465c147
Lily Tran <litran@pdx.edu>
2020-01-27 10:12:08 -0800

    CF1-123 Attempt at getting committee info via id

--- src/components/get-reports/index.js
+++ src/components/get-reports/index.js
@@ -1,23 +1,19 @@
-/*
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Select } from 'antd';
-
-const { Option } = Select;
+import { Descriptions } from 'antd';
 
 class GetReports extends Component {
   constructor(props) {
     super(props);
     this.state = {
       committees: [],
-      commiteeInfo: [],
+      committeeInfo: [],
     };
 
-    this.fetchCommitee = this.fetchFaculty.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.fetchCommittees = this.fetchCommittees.bind(this);
   }
 
-  fetchCommittee() {
+  fetchCommittees() {
     axios
       .get('/api/committees')
       .then(response => {
@@ -35,33 +31,72 @@ class GetReports extends Component {
       });
   }
 
-  handleChange(value) {
-    this.setState({
-      showInfo: true,
-    });
+  fetchCommitteeInfo() {
+    axios
+      .get('/api/committeeInfo/committees')
+      .then(response => {
+        this.setState({
+          committeeInfo: response.data,
+          loading: false,
+          error: {},
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: { message: err.response.data.error, code: err.response.status },
+          loading: false,
+        });
+      });
   }
 
   componentDidMount() {
-    this.fetchFaculty();
+    this.fetchCommittees();
+    this.fetchCommitteeInfo();
   }
 
   render() {
-    const options = this.state.facultyMembers.map(faculty => (
-      <Option key={faculty.email} value={faculty.full_name}>
-        {faculty.full_name}
-      </Option>
-    ));
+    const Name = this.state.committees.map(committee => {
+      return <li key={`committee-${committee.committee_id}`}>{committee.name}</li>;
+    });
+
+    const cInfo = this.state.committeeInfo.map(committeeinfo => {
+      return (
+        <li key={`committee-${committeeinfo.committee_id}`}>
+          {committeeinfo.committee_id}
+        </li>
+      );
+    });
+
+    axios.get(`/api/committeeInfo/${Name}`).then(response => {
+      const Info = response.data;
+      this.setState({ Info });
+    });
+
+    //const Info = this.state.committees.map(committeeinfo => {
+    //  return axios.get(`api/committeeInfo/${committeeinfo.committee_id}`)
+    // });
 
     return (
+      <div>
+        <h1>Get Reports Here</h1>
         <div>
-            <h1>Get committee info here</h1>
-            <div>
-              {options}
-            </div>
+          <Descriptions>
+            <Descriptions.Item label="">{Name}</Descriptions.Item>
+          </Descriptions>
+
+          <Descriptions>
+            <Descriptions.Item label="Description">{}</Descriptions.Item>
+          </Descriptions>
+
+          <Descriptions>
+            <Descriptions.Item label="Total Slots">{}</Descriptions.Item>
+          </Descriptions>
+
+          {/*this.state.Info.map(infos => <li>{infos.Name}</li>)*/}
+          {cInfo}
         </div>
+      </div>
     );
   }
 }
-
 export default GetReports;
-*/
