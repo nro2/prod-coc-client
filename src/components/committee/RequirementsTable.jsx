6cd923255be0f6341e21c9de377d19e011734ffc
Josh Wolsborn <wolsborn@pdx.edu>
2020-02-07 17:39:20 -0800

    CF1-158 Editable Slot Requirements Add edit button Default inputnumber boxes to disabled Move slotreqs inside component - all examples have shown these variables outside of the component but this change was made to allow for the inputnumber component to access state values that will allow it to make http requests with the necessary data.

--- src/components/committee/RequirementsTable.jsx
+++ src/components/committee/RequirementsTable.jsx
@@ -1,42 +1,58 @@
 import React, { Component } from 'react';
-import { Table, Divider, InputNumber } from 'antd';
+import { Table, Divider, InputNumber, Button } from 'antd';
+import axios from 'axios';
 
 const pageSize = 30; // Page size to show pagination
-const reqColumns = [
-  {
-    title: 'Senate',
-    dataIndex: 'senateShortname',
-    editable: false,
-  },
-  {
-    title: 'Filled',
-    dataIndex: 'slotFilled',
-    editable: false,
-  },
-  {
-    title: 'Required',
-    dataIndex: 'slotMinimum',
-    render: value => <InputNumber min={0} defaultValue={value} />,
-  },
-  {
-    title: 'To Be Filled',
-    dataIndex: 'slotsRemaining',
-    editable: false,
-  },
-];
 
 export default class RequirementsTable extends Component {
+  state = {
+    disabled: true,
+  };
+
+  toggle = () => {
+    this.setState({
+      disabled: !this.state.disabled,
+    });
+  };
+
+  reqColumns = [
+    {
+      title: 'Senate',
+      dataIndex: 'senateShortname',
+      editable: false,
+    },
+    {
+      title: 'Filled',
+      dataIndex: 'slotFilled',
+      editable: false,
+    },
+    {
+      title: 'Required',
+      dataIndex: 'slotMinimum',
+      render: value => (
+        <InputNumber min={0} defaultValue={value} disabled={this.state.disabled} />
+      ),
+    },
+    {
+      title: 'To Be Filled',
+      dataIndex: 'slotsRemaining',
+      editable: false,
+    },
+  ];
   render() {
     return (
       <div>
         <Divider type="horizontal" orientation="left">
           Requirements
         </Divider>
+        <Button onClick={this.toggle} type="primary">
+          Edit
+        </Button>
         <Table
           rowKey="senateShortname"
           bordered
           dataSource={this.props.data}
-          columns={reqColumns}
+          columns={this.reqColumns}
           pagination={1 > pageSize && { pageSize }}
           size="small"
         />
